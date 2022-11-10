21b38f9b1b5f6641a7a9826318ed82bef34d1132
chris bao <baoqger@aliyun.com>
2022-11-10 23:00:15 +0800

    add content

--- fileman.c
+++ fileman.c
@@ -3,4 +3,26 @@
  * to manipulate files and their modes.
  * */
 
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include <sys/errno.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
+extern char* getwd();
+
+/* The names of functions that actually do the manipulation. */
+int com_list(), com_view(), com_rename(), com_stat(), com_pwd();
+int com_delete(), com_help(), com_cd(), com_quit();
+
+/*
+ * A structure which contains information on the commands this program can understand
+ * */
+
+typedef struct {
+    char *name; // User printable name of the function
+    rl_icpfunc_t *func; // Function to call to do the job
+    char *doc;
+} COMMAND;
