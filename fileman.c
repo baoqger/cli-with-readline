87052c5de89be1358a660754d8904ef08b870d30
chris bao <baoqger@aliyun.com>
2022-11-10 22:30:23 +0800

    add content

---
+++ fileman.c
@@ -0,0 +1,6 @@
+/* fileman.c -- A tiny application which demonstrates how to use the 
+ * GNU Readline library. This application interactively allows users
+ * to manipulate files and their modes.
+ * */
+
+
