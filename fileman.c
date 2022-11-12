e3f3955abae997881701da32d3dece1e24541aa0
chris bao <baoqger@aliyun.com>
2022-11-12 21:27:24 +0800

    add content

--- fileman.c
+++ fileman.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <ctype.h>
 #include <sys/types.h>
 #include <sys/file.h>
@@ -50,7 +51,8 @@ char* stripwhite(char*);
 COMMAND* find_command(char*);
 void initialize_readline();
 int execute_line(char*);
-
+int valid_argument(char*, char*);
+void too_dangerous(char*);
 
 // The name of this program, as taken from argv[0]
 char *program;
@@ -254,37 +256,116 @@ int com_list(char *arg) {
     return system(syscom);
 }
 
+
+// Change to the directory ARG
 int com_cd(char *arg) {
+    if (chdir(arg) == -1) {
+        perror(arg);
+        return 1;
+    } 
+    com_pwd("");
     return 0;
 }
 
 int com_delete(char *arg) {
-    return 0;
+    too_dangerous("delete");
+    return 1;
 }
 
-
+// print out help for ARG, or for all of the commands if ARG is not present
 int com_help (char *arg) {
+    register int i;
+    int printed = 0;
+    for (i = 0; commands[i].name; i++) {
+        if(!*arg || strcmp(arg, commands[i].name) == 0) {
+            printf("%s\t\t%s.\n", commands[i].name, commands[i].doc);
+            printed++;
+        } 
+    }
+    if (!printed) {
+        printf("No commands match '%s'. Possibilities are: \n", arg);
+        for (i = 0; commands[i].name; i++) {
+            // print in six columns
+            if (printed == 6) {
+                printed = 0;
+                printf("\n");
+            }
+            printf("%s\t", commands[i].name);
+            printed++;
+        }
+        if (printed)
+            printf("\n");
+    }
     return 0;
 }
 
+//Print out the current working directory
 int com_pwd(char* arg) {
+    char dir[1024];
+    if (getcwd(dir, sizeof(dir)) == NULL) {
+        printf("Error getting pwd: %s\n", dir);
+        return 1;
+    }
+    printf("Current directory is %s\n", dir);
     return 0;
 }
 
+// The user wishes to quit using this program. Just set DONE non-zero
 int com_quit(char* arg) {
+    done = 1;
     return 0;
 }
 
 int com_rename(char *arg) {
-    return 0;
+    too_dangerous("rename");
+    return 1;
 }
 
 int com_stat(char *arg) {
+    struct stat finfo;
+
+    if (!valid_argument("stat", arg))
+        return 1;
+
+    if (stat(arg, &finfo) == -1) {
+        perror(arg);
+        return 1;
+    }
+    printf("Statistics for '%s': \n", arg);
+
+    printf("%s has %lu link%s, and is %lu byte%s in length.\n", arg,
+            finfo.st_nlink,
+            (finfo.st_nlink == 1) ? "" : "s",
+            finfo.st_size,
+            (finfo.st_size == 1) ? "" : "s"
+            );
+    printf("Inode Last change at: %s", ctime((time_t *)&finfo.st_ctim));
+    printf("      Last access at: %s", ctime((time_t *)&finfo.st_atime));
+    printf("      Last modified at: %s\n", ctime((time_t *)&finfo.st_mtim));
     return 0;
 }
 
 int com_view(char *arg) {
-    return 0;
+    if (!valid_argument("view", arg)) 
+        return 1;
+    sprintf(syscom, "more %s", arg);
+    return system(syscom);
+}
+
+// Return non-zero if ARG is a valid argument for CALLER, else print
+// an error message and return zero
+int valid_argument(char *caller, char* arg) {
+    if (!arg || !*arg) {
+        fprintf(stderr, "%s: Argument required.\n", caller);
+        return 0;
+    }
+    return 1;
+}
+
+
+// Function which tells you that you can't do this
+void too_dangerous(char *caller) {
+    fprintf(stderr, "%s: Too dangerous for me to distribute. Write it yourself.\n", caller);
 }
 
 
